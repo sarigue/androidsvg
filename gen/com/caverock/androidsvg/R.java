d4142e2d3d6bda9f41682dd999f54dbc4a11e810
Francois RAOULT <fra.raoult@gmail.com>
2016-03-11 01:11:47 +0100

    Refactoring

--- gen/com/caverock/androidsvg/R.java
+++ gen/com/caverock/androidsvg/R.java
@@ -9,8 +9,64 @@ package com.caverock.androidsvg;
 
 public final class R {
     public static final class attr {
+        /** <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+         */
+        public static int background=0x7f010001;
+        /**  Location of the SVG document. 
+         <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+         */
+        public static int svg=0x7f010000;
     }
     public static final class drawable {
         public static int checkerboard=0x7f020000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a SVGImageView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #SVGImageView_background com.caverock.androidsvg:background}</code></td><td></td></tr>
+           <tr><td><code>{@link #SVGImageView_svg com.caverock.androidsvg:svg}</code></td><td> Location of the SVG document.</td></tr>
+           </table>
+           @see #SVGImageView_background
+           @see #SVGImageView_svg
+         */
+        public static final int[] SVGImageView = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.caverock.androidsvg.R.attr#background}
+          attribute's value can be found in the {@link #SVGImageView} array.
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+          @attr name com.caverock.androidsvg:background
+        */
+        public static final int SVGImageView_background = 1;
+        /**
+          <p>
+          @attr description
+           Location of the SVG document. 
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+          <p>This is a private symbol.
+          @attr name com.caverock.androidsvg:svg
+        */
+        public static final int SVGImageView_svg = 0;
+    };
 }
